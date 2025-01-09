54ea06812b59b03d9b80756f8f14ab7baba837fc
muathzoubi <moathaza@gmail.com>
2025-01-09 14:24:44 -0800

    kd

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,35 +15,39 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ar" dir="rtl">
-      <body className=" overflow-auto" style={{zoom:0.8}}>
-      <main className=''>
-      {children}
-      </main>
+      <body className=" overflow-auto" style={{ zoom: 0.8 }}>
+        <main className=''>
+          {children}
+        </main>
         {/* Bottom Navigation */}
         <nav className="fixed bottom-0 left-0 right-0 bg-[#001838] text-white p-4">
-        <div className="flex justify-between items-center max-w-md mx-auto">
-          <button className="flex flex-col items-center gap-1">
-            <User className="w-6 h-6" />
-            <span className="text-xs">لوحة التحكم</span>
-          </button>
-          <Link href={'/travels'}>
-          <button className="flex flex-col items-center gap-1">
-            <Briefcase className="w-6 h-6" />
-            <span className="text-xs">حجوزاتي</span>
-          </button>
-          </Link>
-          <Link href="/offres">
-          <button className="flex flex-col items-center gap-1">
-            <Tag className="w-6 h-6" />
-            <span className="text-xs">العروض</span>
-          </button>
-          </Link>
-          <button className="flex flex-col items-center gap-1 ">
-            <Home className="w-6 h-6" />
-            <span className="text-xs">الصفحة الرئيسية</span>
-          </button>
-        </div>
-      </nav></body>
+          <div className="flex justify-between items-center max-w-md mx-auto">
+            <button className="flex flex-col items-center gap-1">
+              <User className="w-6 h-6" />
+              <span className="text-xs">لوحة التحكم</span>
+            </button>
+            <Link href={'/travels'}>
+              <button className="flex flex-col items-center gap-1">
+                <Briefcase className="w-6 h-6" />
+                <span className="text-xs">حجوزاتي</span>
+              </button>
+            </Link>
+            <Link href="/offres">
+              <button className="flex flex-col items-center gap-1">
+                <Tag className="w-6 h-6" />
+                <span className="text-xs">العروض</span>
+              </button>
+            </Link>
+            <Link href="/">
+
+              <button className="flex flex-col items-center gap-1 ">
+                <Home className="w-6 h-6" />
+                <span className="text-xs">الصفحة الرئيسية</span>
+              </button>
+            </Link>
+          </div>
+
+        </nav></body>
     </html>
   );
 }
